735d0beaf231f59dd85e60a9254774eac8c295bd
nagashree <naagashree42@gmail.com>
2026-06-20 19:20:00 +0530

    done

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,16 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	int a=20;
+	int b=40;
+	int sum=a+b;
+        System.out.println( "Sum of a and b is" +sum );
+    }
+}
